9b2fe749fb882d1ee621b28928d8d3aee2547f91
sshaul <sshaul@calpoly.edu>
2017-01-14 11:25:24 -0800

    Changed dictionary in queries.py

--- server/queries.py
+++ server/queries.py
@@ -13,7 +13,16 @@ class Database:
       self.cur = self.db.cursor()
 
    def UserInfo(self, email):
+      resDict = {}
       query = "SELECT * FROM Users WHERE email = %s;"
       self.cur.execute(query, (email,))
       res = self.cur.fetchall()
-      return jsonify(res)
+      resDict{
+         'id': res[0], 
+         'email': res[1], 
+         'password': res[2],
+         'token': res[3],
+         'picture': res[4],
+         'role': res[5]
+         }
+      return jsonify(resDict)
