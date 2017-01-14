5a304b81a013ab518dc589abd2545882c34cf7d6
sshaul <sshaul@calpoly.edu>
2017-01-14 11:06:57 -0800

    Added queries.py

---
+++ server/queries.py
@@ -0,0 +1,19 @@
+import MySQLdb
+from flask import jsonify
+
+# Database class
+class Database:
+
+   # Connect to database and create cursor.
+   def __init__(self):
+      self.db = MySQLdb.connect(host = "ajkprojects.com",
+                                user = "client",
+                                passwd = "polyhacks",
+                                db = "RideRight")
+      self.cur = self.db.cursor()
+
+   def UserInfo(self, email):
+      query = "SELECT * FROM Users WHERE email = %s;"
+      self.cur.execute(query, (email))
+      res = self.cur.fetchall()
+      return jsonify(res)
