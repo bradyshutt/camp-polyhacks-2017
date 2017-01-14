522bca477f6494820233335877fd41fb99d2c7f1
Brady Shutt <bradyshutt@gmail.com>
2017-01-14 11:00:31 -0800

    structure

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MaterialModule, MdIconRegistry } from '@angular/material';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
+import { MenuComponent } from './menu.component';
+import { ToDowntownComponent } from './to-downtown.component';
+import { FromDowntownComponent } from './from-downtown.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot(),
+    MaterialModule.forRoot(),
+  ],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    ToDowntownComponent,
+    FromDowntownComponent,
   ],
   providers: [],
   bootstrap: [ AppComponent ]
---
+++ front-end/src/app/to-downtown.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-downtown',
+  template: `
+    <p>
+      to-downtown Works!
+    </p>
+  `,
+  styles: []
+})
+export class ToDowntownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
