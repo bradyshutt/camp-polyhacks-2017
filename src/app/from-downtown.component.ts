e3895a13922055c798daf46500a871f2a555451e
Brady Shutt <bradyshutt@gmail.com>
2017-01-14 19:27:16 -0800

    .

--- src/app/from-downtown.component.ts
+++ src/app/from-downtown.component.ts
@@ -3,11 +3,81 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-from-downtown',
   template: `
-    <p>
-      from-downtown Works!
-    </p>
+<div>
+  <div align = "center" overflow="hidden">
+    <div>
+      <img class = "header" src = "../assets/fromdt.png" style = "float: right;">
+      <input type="text" readonly id="pickupLoc" placeholder="Pick-Up Location: "> 
+      <img class = "mapimg" src = "../assets/map2.png">
+      <input type="text" id="destination" placeholder = "Enter Destination" name="destination">
+      <p> 
+        <input type="submit" value="Submit Request" > <br>
+          <button class="back" value="Go Back" >Go Back</button> <br>
+      </p> 
+
+  </div>
+  <div class = "space"> </div>   
+  </div>
+<div class = "space"></div>
+
+</div>
   `,
-  styles: []
+
+  styles: [`
+
+.space {padding: 150px;}
+.nspace {padding: 5px;}
+.regimg {
+  opacity: 0.7;
+  position: relative;
+  width: 15%;
+  height: 15%;
+}
+.header {
+  width: 100%;
+  height: 25%;
+}
+.mapimg {
+  width: 50%; 
+}
+body {
+  height: 870px;
+  width: 900px;
+  position: absolute;
+  background-color: #f2f2f2;
+  padding: 10px;
+}
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px 10px 10px ;
+    margin: 8px 8px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    font-size: 16px;
+}
+input[type=submit], select {
+  font-size: 16px;
+}
+p.sans {
+  font-family: 'PT Sans', sans-serif;
+}
+input[type=submit] {
+    width: 100%;
+    background-color: #E04C5B;
+    color: white;
+    padding: 14px 20px 10px 10px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+input[type=submit]:hover {
+    background-color: #a9a9a9;
+}
+
+    `]
 })
 export class FromDowntownComponent implements OnInit {
 
--- src/app/to-downtown.component.ts
+++ src/app/to-downtown.component.ts
@@ -3,24 +3,89 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-to-downtown',
   template: `
-     <button class="round-button" type="button">Current Location</button> <br/>
-     <select>
-        <option value = "1">1</option>
-        <option value = "2">2</option>
-        <option value = "3">3</option>
-     </select>
+  <div>
+    <div align="center" overflow="hidden">
+      <div>
+        <img class="header-img" src = "../assets/todt.png" style="float:right;">
+        <input type="text" id="pickupLoc" placeholder="Current Location: ">  
+        <img class="mapimg" src="../assets/map2.png">
+        <input type="text" id="destination" placeholder="Enter Destination" name="destination"> 
+        <p>
+          <input type="text" placeholder="Number of People" name="numOfPeople"/>
+          <input type="submit" value="Submit Request" > <br>
+          <button class="back" value="Go Back" >Go Back</button> <br>
+        </p> 
+      </div> 
+  </div>
+</div>
   `,
   styles: [`
-    .size{
-       height: 30px;
-       width: 400px;
-    }
-    .round-button{
-       border-radius: 15px;
-       background: #E04C5B;
-       width: 400px;
-       height: 30px;
-    }
+
+.regimg {
+  opacity: 0.7;
+  position: relative;
+  width: 20%;
+  height: 20%;
+}
+
+.mapimg {
+  width: 100%; 
+  height: 50%;
+  display: block;
+}
+.header-img {
+    margin: 5px 0;
+    width: 100%;
+}
+
+body {
+  height: 870px;
+  width: 900px;
+  position: absolute;
+  background-color: #f2f2f2;
+  padding: 10px;
+}
+
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px 10px 10px ;
+    margin: 8px 8px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    font-size: 16px;
+}
+input[type=submit], select {
+  font-size: 16px;
+}
+p.sans {
+  font-family: 'PT Sans', sans-serif;
+}
+
+input[type=submit] {
+    width: 100%;
+    background-color: #E04C5B;
+    color: white;
+    padding: 14px 20px 10px 10px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+input[type=submit]:hover {
+    background-color: #a9a9a9;
+}
+.back {
+    background-color: deepskyblue;
+    width: 100%;
+    color: white;
+    padding: 14px 20px 10px 10px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
 `]
 })
 export class ToDowntownComponent implements OnInit {
